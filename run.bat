07e4f4fdf2d27799a6ced616602f5c69001a7e34
matyi25 <t.matyi@tomolya.net>
2015-05-15 12:28:17 +0200

    Update scramble.py and new file run.bat
    Unit test added to scramble python module. Also the run.bat added, which
    is a batch file to run easily the module.

---
+++ run.bat
@@ -0,0 +1,1 @@
+python scramble.py -v
--- scramble.py
+++ scramble.py
@@ -1,16 +1,59 @@
 
 def scrambleTheWords(inputString):
+    """Return the scrambled version of the input string. 
+    From each word the first and the last charachter stays on the place
+    the remaining characters are read in reversed order.
+    For instance:
+    
+    thing - t nih g
+    
+    Works even on more sentences, not just one. 
+    >>> scrambleTheWords("thing")
+    'tnihg'
+    >>> scrambleTheWords("Its okay really.")
+    'Its oaky rllaey.'
+    >>> scrambleTheWords("Its okay. Even with two sentences.")
+    'Its oaky. Eevn wtih two secnetnes.'
+    >>> scrambleTheWords("")
+    Traceback (most recent call last):
+        ...
+    ValueError: inputString cannot be empty
+    >>> scrambleTheWords(None)
+    Traceback (most recent call last):
+        ...
+    ValueError: inputString cannot be None
+    """
+    if inputString == None:
+        raise ValueError("inputString cannot be None")
+    if len(inputString)==0:
+        raise ValueError("inputString cannot be empty")
+    
+    
+    
     scrambledString = ""
     words = inputString.split(" ")
     temp = ""
+    
     for word in words:
-        temp = word[1:-1]
-        if len(word) > 1:
-            scrambledString = scrambledString + word[0] + temp[::-1] + word[-1] + " "
-        else:
-            scrambledString = scrambledString + word[0] + " "
+        if word[-1] == ".":
+            temp = word[1:-2]
+            if len(word) > 2:
+                scrambledString = scrambledString + word[0] + temp[::-1] + word[-2] + word[-1]
+            else:
+                scrambledString = scrambledString + word[0] + word[+1]
             
+        else:
+            temp = word[1:-1]
+            if len(word) > 1:
+                scrambledString = scrambledString + word[0] + temp[::-1] + word[-1]
+            else:
+                scrambledString = scrambledString + word[0]
+                
+        if words.index(word) != (len(words)-1):
+            scrambledString = scrambledString + " "   
+               
     return scrambledString
 
-print(scrambleTheWords("Atraktam ezt a szavat"))
-    
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
